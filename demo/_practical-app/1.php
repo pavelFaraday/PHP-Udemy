744198fddf91880cecbd7942974116820fb950c2
pavelFaraday <devstudio.corp@gmail.com>
2021-11-10 11:40:52 +0400

    PHP Udemy | 9 - Practice Section 1

--- demo/_practical-app/1.php
+++ demo/_practical-app/1.php
@@ -1,37 +1,31 @@
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
+<?php include "includes/header.php"; ?>
 
-	<section class="content">
+<section class="content">
 
 	<aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
-			
-	</aside><!--SIDEBAR-->
+		<?php Navigation(); ?>
+
+	</aside>
+	<!--SIDEBAR-->
 
 
-<article class="main-content col-xs-8">
-		
+	<article class="main-content col-xs-8">
 
+		<?php
 
-		<?php  
+		// Step 1:  Use the Echo Function to say hello with html h1 tags embedded inside php.
+		// Step 2: Write a comment above the echo function and explain what that function is doing.
 
-
-		/* 
-		   Step 1:  Use the Echo Function to say hello with html h1                  tags embedded inside php.
-
-
-		   Step 2: Write a comment above the echo function and explain
-		   what that function is doing.
-
-
-		   */
+		echo '<h1>Hello - I am PHP.</h1>';
 
 		?>
 
-	
+		<h2><?php echo 'Hello Giorgi' ?></h2>
 
-		</article><!--MAIN CONTENT-->
 
-<?php include "includes/footer.php"; ?>
+	</article>
+	<!--MAIN CONTENT-->
+
+	<?php include "includes/footer.php"; ?>
