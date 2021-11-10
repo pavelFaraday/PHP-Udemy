d0aa73131953e73171f1fa425551530b77916e9b
pavelFaraday <devstudio.corp@gmail.com>
2021-11-10 11:25:34 +0400

    PHP Udemy | 8 - inserting Comments in PHP

---
+++ myCode/8_comments.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comments in PHP</title>
+</head>
+
+<body>
+    <?php
+    // This is a Comment in PHP
+
+    /* This 
+    is 
+    multyline
+    code
+    comment 
+    */
+
+    echo 'My name is Giorgi';
+    ?>
+
+</body>
+
+</html>
