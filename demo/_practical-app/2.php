7b3cc6f6a1c6a466c4e78d20587bf79ad9523d5f
pavelFaraday <devstudio.corp@gmail.com>
2021-11-11 14:57:16 +0400

    PHP Udemy | Practical App - Section 2

--- demo/_practical-app/2.php
+++ demo/_practical-app/2.php
@@ -4,38 +4,30 @@
 <section class="content">
 
 	<aside class="col-xs-4">
-
 		<?php Navigation(); ?>
-
-
 	</aside>
-	<!--SIDEBAR-->
 
-
+	<!--MAIN CONTENT-->
 	<article class="main-content col-xs-8">
-
 
 		<?php
 
+		/* 
+		Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
+		Step 2: Add the two variables and display the sum with echo:
+		Step3: Make 2 Arrays with the same values, one regular and the other associative 
+		*/
 
+		$number1 = 10;
+		$number2 = 20;
+		echo '<h2>' . ($number1 + $number2) . '</h2>' . '<br>';
 
-		/* Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
-
-		  Step 2: Add the two variables and display the sum with echo:
-
-
-		  Step3: Make 2 Arrays with the same values, one regular and the other associative
-
-		 
-			 */
-
-
-
+		$arr1 = ['glam', 'Pablo', 23, '<h3>Hello</h3>'];
+		$arr2 = ['word' => 'glam', 'name' => 'Pablo', 'age' => 23, 'tag' => '<h3 style="color:red;">Hello</h3>'];
+		echo $arr1[1] . '<br>';
+		echo $arr2['tag'];
 
 		?>
-
-
-
 	</article>
 	<!--MAIN CONTENT-->
 
