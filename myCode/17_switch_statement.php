d66f69324e1c259b75c36125e10ab0191209e612
pavelFaraday <devstudio.corp@gmail.com>
2021-11-12 14:51:45 +0400

    PHP Udemy | 17 - Switch Statements in PHP

---
+++ myCode/17_switch_statement.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>17. Switch Statement | PHP Udemy</title>
+</head>
+
+<body>
+    <?php
+    $number = 10;
+
+    switch ($number) {
+        case 35:
+            echo '10 is 35';
+            break;
+        case 20:
+            echo '10 is 20';
+            break;
+        case 10:
+            echo '10 is 10';
+            break;
+        default:
+            echo 'I Dont Know';
+            break;
+    }
+    ?>
+</body>
+
+</html>
