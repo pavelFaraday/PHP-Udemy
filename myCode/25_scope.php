579bc6880a0a89c45d53b573bc26f6c467feb1bc
pavelFaraday <devstudio.corp@gmail.com>
2021-11-13 14:34:57 +0400

    PHP Udemy | modify README.md

--- myCode/25_scope.php
+++ myCode/25_scope.php
@@ -19,11 +19,16 @@
         $x = 15;
     }
 
+    $scope_link_PHP = 'https://www.php.net/manual/en/language.variables.scope.php';
+
     ?>
+
 
     <h1><?php echo $x; ?></h1> <!-- 100 -->
     <h2><?php convert(); ?></h2> <!-- with this function local variable - $x is converted/modified to global -->
     <h3><?php echo $x; ?></h3> <!-- 15 -->
+
+    <a href="<?php echo $scope_link_PHP; ?>" target="_blank">See Scope in PHP Official Guide</a>
 </body>
 
 </html>
