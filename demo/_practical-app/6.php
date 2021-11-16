811779f4f1ae79b20a9f77bb30914a9af1336e0f
pavelFaraday <devstudio.corp@gmail.com>
2021-11-16 17:02:36 +0400

    PHP Udemy | Practical App - Section 6

--- demo/_practical-app/6.php
+++ demo/_practical-app/6.php
@@ -1,29 +1,50 @@
-
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
+<?php include "includes/header.php"; ?>
 
-	<section class="content">
+<section class="content">
 
-		<aside class="col-xs-4">
-		
-		<?php Navigation();?>
-			
-		</aside><!--SIDEBAR-->
+	<aside class="col-xs-4">
 
+		<?php Navigation(); ?>
 
-<article class="main-content col-xs-8">
- 
-
-	<?php  
-
-/*  Step1: Make a form that submits one value to POST super global
+	</aside>
+	<!--SIDEBAR-->
 
 
- */
+	<article class="main-content col-xs-8">
 
-	
-?>
+		<?php
+		if (isset($_POST['submit'])) {
+			$username = $_POST['name'];
+			$userAge = $_POST['age'];
+			$customer_list = ['Johnatan', 'Banjove', 'Michel', 'Vanesa']; // image this is customer list in DB
+
+			$min_str_len = 5;
+			$max_str_len = 15;
+
+			if (strlen($username) < $min_str_len) {
+				echo '<br> Username length should be more then 5! <br>';
+			} elseif (strlen($username) > $max_str_len) {
+				echo '<br> Username length should be less then 15! <br>';
+			} elseif (!in_array($username, $customer_list)) {
+				echo '<br>' . $username . ',' . ' you are not registred!';
+			} else {
+				echo 'Hello ' . $username . '!' . '<br>';
+				echo 'Your age is ' . $userAge;
+			}
+
+			exit();
+		}
+		?>
+
+		<form action="<?php $_PHP_SELF ?>" method="POST">
+			<input type="text" name="name" />
+			<input type="number" name="age" />
+			<input type="submit" name="submit" />
+		</form>
 
 
-</article><!--MAIN CONTENT-->
-<?php include "includes/footer.php"; ?>
+
+	</article>
+	<!--MAIN CONTENT-->
+	<?php include "includes/footer.php"; ?>
