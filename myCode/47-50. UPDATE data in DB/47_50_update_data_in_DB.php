473ba0866a0ee6e9ffc51f982976266804092fb1
pavelFaraday <devstudio.corp@gmail.com>
2021-12-08 19:12:24 +0400

    PHP Udemy | 47-50. UPDATE data in DB

--- myCode/47-50. UPDATE data in DB/47_50_update_data_in_DB.php
+++ myCode/47-50. UPDATE data in DB/47_50_update_data_in_DB.php
@@ -4,22 +4,7 @@ include "connection_DB.php";
 include "functions.php";
 
 if (isset($_POST['submit'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];;
-    $id = $_POST['id'];
-
-    $queryUpdate = "UPDATE users SET ";
-    $queryUpdate .= "username = '$username', ";
-    $queryUpdate .= "password = '$password' ";
-    $queryUpdate .= "WHERE id = '$id'";
-
-    $result = mysqli_query($connection, $queryUpdate);
-
-    if (!$result) {
-        die('QUERY FAILED! ' . mysqli_error($connection));
-    } else {
-        echo "Data Saved!";
-    }
+    updateData();
 }
 
 ?>
--- myCode/47-50. UPDATE data in DB/functions.php
+++ myCode/47-50. UPDATE data in DB/functions.php
@@ -13,3 +13,24 @@ function showAllData()
         echo "<option value='$id'>$id</option>";
     }
 }
+
+function updateData()
+{
+    global $connection;
+    $username = $_POST['username'];
+    $password = $_POST['password'];;
+    $id = $_POST['id'];
+
+    $queryUpdate = "UPDATE users SET ";
+    $queryUpdate .= "username = '$username', ";
+    $queryUpdate .= "password = '$password' ";
+    $queryUpdate .= "WHERE id = '$id'";
+
+    $result = mysqli_query($connection, $queryUpdate);
+
+    if (!$result) {
+        die('QUERY FAILED! ' . mysqli_error($connection));
+    } else {
+        echo "Data Saved!";
+    }
+}
