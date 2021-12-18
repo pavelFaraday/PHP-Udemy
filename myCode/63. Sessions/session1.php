6337415090f248ca68b67d42d5ee26687dde8859
pavelFaraday <devstudio.corp@gmail.com>
2021-12-18 20:57:04 +0400

    PHP Udemy | 63 Sessions

---
+++ myCode/63. Sessions/session1.php
@@ -0,0 +1,24 @@
+<?php
+// sessions are for transferiong/caring information from one to another page. 
+
+session_start();
+$_SESSION["greeting"] = "Hello Student";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>63. Sessions</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
