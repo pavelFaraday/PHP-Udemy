c8566e03c7246e56b98189c80ba2c5f3d1df8b00
pavelFaraday <devstudio.corp@gmail.com>
2022-02-02 14:07:20 +0400

    PHP Udemy - Blog || 123. Setting up some links and making an excerpt || substr()

--- Blog/cms/category.php
+++ Blog/cms/category.php
@@ -5,7 +5,6 @@
 <!-- Navigation -->
 <?php include 'includes/navigation.php'; ?>
 
-
 <!-- Page Content -->
 <div class="container">
     <div class="row">
@@ -27,7 +26,7 @@
                 $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
                 $post_image = $row['post_image'];
-                $post_content = $row['post_content'];
+                $post_content = substr($row['post_content'], 0, 150);
 
             ?>
 
