53dc923c84891ed5ef38e9745c5ad9cd73c67c6a
pavelFaraday <devstudio.corp@gmail.com>
2022-02-09 15:46:04 +0400

    PHP Udemy - Blog || 133. Approving and unapproving comments

--- Blog/cms/admin/includes/view_all_comments.php
+++ Blog/cms/admin/includes/view_all_comments.php
@@ -59,8 +59,8 @@
 
                 echo "<td>{$comment_date}</td>";
 
-                echo "<td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>";
-                echo "<td><a href='posts.php?delete='>Unapprove</a></td>";
+                echo "<td><a href='comments.php?approve=$comment_id'>Approve</a></td>";
+                echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";
                 echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
                 echo "</tr>";
             }
@@ -71,6 +71,25 @@
 
 <?php
 
+// approve
+if (isset($_GET['approve'])) {
+    $the_comment_id = $_GET['approve'];
+
+    $query = "UPDATE comments SET comment_status = 'approve' WHERE comment_id = $the_comment_id ";
+    $approve_comment_query = mysqli_query($connection, $query);
+    header("Location: comments.php");
+}
+
+// unapprove
+if (isset($_GET['unapprove'])) {
+    $the_comment_id = $_GET['unapprove'];
+
+    $query = "UPDATE comments SET comment_status = 'unapprove' WHERE comment_id = $the_comment_id ";
+    $unapprove_comment_query = mysqli_query($connection, $query);
+    header("Location: comments.php");
+}
+
+// Delete
 if (isset($_GET['delete'])) {
     $the_comment_id = $_GET['delete'];
 
