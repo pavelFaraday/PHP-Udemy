7c1b74bc1b4bd3dd5d8b20ecdf067c39b7077e2a
pavelFaraday <devstudio.corp@gmail.com>
2022-02-10 15:42:02 +0400

    PHP Udemy - Blog || 135. Increasing comments count

--- Blog/cms/post.php
+++ Blog/cms/post.php
@@ -69,6 +69,11 @@
                 if (!$create_omment_query) {
                     die('QUERY FAILED!' . mysqli_error($connection));
                 }
+
+                $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
+                $query .= "WHERE post_id = $the_post_id ";
+
+                $update_comment_count = mysqli_query($connection, $query);
             }
             ?>
 
