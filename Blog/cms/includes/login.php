e99987879fc4859326889f9c13e0137909bf1645
pavelFaraday <devstudio.corp@gmail.com>
2022-02-16 15:15:51 +0400

    PHP Udemy - Blog || 157. Set Values with Sessions

--- Blog/cms/includes/login.php
+++ Blog/cms/includes/login.php
@@ -1,5 +1,7 @@
 <?php include "db.php";
 
+session_start();
+
 if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -26,6 +28,11 @@ if (isset($_POST['login'])) {
     if ($username !== $db_username && $password !== $db_user_password) {
         header("Location: ../index.php");
     } else if ($username == $db_username && $password == $db_user_password) {
+        $_SESSION['username'] = $db_username;
+        $_SESSION['firstname'] = $db_user_firstname;
+        $_SESSION['lastname'] = $db_user_lastname;
+        $_SESSION['user_role'] = $db_user_role;
+
         header("Location: ../admin");
     } else {
         header("Location: ../index.php");
