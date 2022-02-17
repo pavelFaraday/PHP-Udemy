2068550e4e57d2d80adf15c721069d65c7acbd92
pavelFaraday <devstudio.corp@gmail.com>
2022-02-17 14:27:26 +0400

    PHP Udemy - Blog || 163. Update User Data

--- Blog/cms/admin/profile.php
+++ Blog/cms/admin/profile.php
@@ -1,7 +1,6 @@
 <?php include 'includes/admin_header.php'; ?>
 
 <?php
-
 if (isset($_SESSION['username'])) {
     $username = $_SESSION['username'];
 
@@ -18,6 +17,38 @@ if (isset($_SESSION['username'])) {
         $user_image = $row['user_image'];
         $user_role = $row['user_role'];
     }
+}
+?>
+
+<?php
+
+if (isset($_POST['edit_user'])) {
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_role = $_POST['user_role'];
+
+    // $post_image = $_FILES['post_image']['name'];
+    // $post_image_temp = $_FILES['post_image']['tmp_name'];
+
+    $username = $_POST['username'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+    // $post_date = date('d-m-y');
+
+    // Upload image from temporary location on the server (folder we specify)
+    // move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $query = "UPDATE users SET ";
+    $query .= "user_firstname = '{$user_firstname}', ";
+    $query .= "user_lastname = '{$user_lastname}', ";
+    $query .= "user_role = '{$user_role}', ";
+    $query .= "username = '{$username}', ";
+    $query .= "user_email = '{$user_email}', ";
+    $query .= "user_password = '{$user_password}' ";
+    $query .= "WHERE username='{$username}' ";
+
+    $edit_user_query = mysqli_query($connection, $query);
+    confirmQuery($edit_user_query);
 }
 
 ?>
@@ -37,7 +68,6 @@ if (isset($_SESSION['username'])) {
                     <small>Author</small>
                 </h1>
 
-                <!-- ---- NEW FORM --- -->
                 <form action="" method="post" enctype="multipart/form-data">
                     <div class="form-group">
                         <label for="title">Firstname</label>
@@ -82,11 +112,7 @@ if (isset($_SESSION['username'])) {
                     <div class="form-group">
                         <input class="btn btn-primary" type="submit" name="edit_user" value="Update User">
                     </div>
-
                 </form>
-
-                <!-- ---- NEW FORM --- -->
-
 
             </div>
             <!-- /.row -->
