3771ec2f5f30b31eb2c1daf85337c3f170e80851
pavelFaraday <devstudio.corp@gmail.com>
2022-02-18 16:20:08 +0400

    PHP Udemy - Blog || 169. Cleane up the chart a little

--- Blog/cms/admin/index.php
+++ Blog/cms/admin/index.php
@@ -143,17 +143,20 @@
 
                     function drawChart() {
                         var data = google.visualization.arrayToDataTable([
-                            ['Year', 'Sales', 'Expenses', 'Profit'],
-                            ['2014', 1000, 400, 200],
-                            ['2015', 1170, 460, 250],
-                            ['2016', 660, 1120, 300],
-                            ['2017', 1030, 540, 350]
+                            ['Data', 'Count'],
+
+                            <?php
+
+
+                            ?>
+
+                            ['Posts', 1000],
                         ]);
 
                         var options = {
                             chart: {
-                                title: 'Company Performance',
-                                subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                                title: 'Blog Performance',
+                                subtitle: 'Posts, Comments, Users, Categories',
                             }
                         };
 
@@ -163,7 +166,7 @@
                     }
                 </script>
 
-                <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+                <div id="columnchart_material" style="width: auto; height: 500px;"></div>
                 <!-- Google Charts -->
 
             </div>
