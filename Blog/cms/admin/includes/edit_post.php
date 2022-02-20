f220c59a59d2d71117cb46913b836644c4e17b34
pavelFaraday <devstudio.corp@gmail.com>
2022-02-20 15:42:18 +0400

    PHP Udemy - Blog || Refactoring

--- Blog/cms/admin/includes/edit_post.php
+++ Blog/cms/admin/includes/edit_post.php
@@ -92,18 +92,16 @@ if (isset($_POST['update_post'])) {
     <div class="form-group">
         <select name="post_status">
             <option value="<?php echo $post_status; ?>"><?php echo $post_status; ?></option>
+
             <?php
             if ($post_status == 'published') {
                 echo "<option value='draft'>Draft</option>";
             } else {
                 echo "<option value='published'>Publish</option>";
             }
-
             ?>
         </select>
     </div>
-
-
 
     <!-- <div class="form-group">
         <label for="post_status">Post Status</label>
