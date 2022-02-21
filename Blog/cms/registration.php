8f81a3fe03dcb714a3aa82465a6721d7a34931bf
pavelFaraday <devstudio.corp@gmail.com>
2022-02-21 13:49:44 +0400

    PHP Udemy - Blog || 195. Extracting Form Values and Escaping

--- Blog/cms/registration.php
+++ Blog/cms/registration.php
@@ -3,6 +3,13 @@
 
 <?php
 if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $username = mysqli_real_escape_string($connection, $username);
+    $email = mysqli_real_escape_string($connection, $email);
+    $password = mysqli_real_escape_string($connection, $password);
 }
 ?>
 
