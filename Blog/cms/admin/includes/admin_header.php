42d8046d11b923a2fc15ff16c12f60fd9ff39515
pavelFaraday <devstudio.corp@gmail.com>
2022-04-05 18:21:42 +0400

    PHP Udemy - Blog | 211. Adding a LOADER to the CMS Admin

--- Blog/cms/admin/includes/admin_header.php
+++ Blog/cms/admin/includes/admin_header.php
@@ -45,6 +45,7 @@ if (!isset($_SESSION['user_role'])) {
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    <link rel="stylesheet" href="css/styles.css">
     <!-- Google Charts -->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
--- Blog/cms/admin/js/scripts.js
+++ Blog/cms/admin/js/scripts.js
@@ -14,6 +14,17 @@ $(document).ready(function() {
         });
       }
     })
+
+
+    let div_box = "<div id='load-screen'><div id='loading'></div></div>";
+    $("body").prepend(div_box);
+
+    $("#load-screen").delay(500).fadeOut(400, function(){
+      $(this).remove();
+    })
+
+
+
   });
 
 
