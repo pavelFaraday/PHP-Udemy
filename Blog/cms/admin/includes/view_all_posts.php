0bb1abdc4137c506e78b6f7a26e65b7ba745cbcc
pavelFaraday <devstudio.corp@gmail.com>
2022-04-06 12:31:32 +0400

    PHP Udemy - Blog | 213. Resetting Views Feature

--- Blog/cms/admin/includes/view_all_posts.php
+++ Blog/cms/admin/includes/view_all_posts.php
@@ -122,7 +122,7 @@ if (isset($_POST['checkBoxArray'])) {
                 echo "<td>{$post_tags}</td>";
                 echo "<td>{$post_comment_count}</td>";
                 echo "<td>{$post_date}</td>";
-                echo "<td>{$post_views_count}</td>";
+                echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
                 echo "<td><a href='../post.php?p_id={$post_id}' target='_blank'>View Post</a></td>";
                 echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                 echo "<td><a onClick=\"javascript: return confirm('Are you sure?'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
@@ -141,6 +141,14 @@ if (isset($_GET['delete'])) {
 
     $query = "DELETE FROM posts WHERE post_id = {$the_post_id} ";
     $query_delete = mysqli_query($connection, $query);
+    header("Location: posts.php");
+}
+
+if (isset($_GET['reset'])) {
+    $the_post_id = $_GET['reset'];
+
+    $query = "UPDATE posts SET post_views_count = 0 WHERE post_id =" . mysqli_real_escape_string($connection, $_GET['reset']) . " ";
+    $reset_query = mysqli_query($connection, $query);
     header("Location: posts.php");
 }
 
