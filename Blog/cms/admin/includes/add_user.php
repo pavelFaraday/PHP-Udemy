6c32daae4e3b8750a7e9e9546a18c98dd542a0d0
pavelFaraday <devstudio.corp@gmail.com>
2022-04-22 14:15:08 +0400

    PHP Udemy - Blog | 229. Adding New System to Add Users in Admin

--- Blog/cms/admin/includes/add_user.php
+++ Blog/cms/admin/includes/add_user.php
@@ -3,14 +3,13 @@ if (isset($_POST['create_user'])) {
     $user_firstname = $_POST['user_firstname'];
     $user_lastname = $_POST['user_lastname'];
     $user_role = $_POST['user_role'];
-    // $post_image = $_FILES['post_image']['name'];
-    // $post_image_temp = $_FILES['post_image']['tmp_name'];
+
+
     $username = $_POST['username'];
     $user_email = $_POST['user_email'];
     $user_password = $_POST['user_password'];
-    // $post_date = date('d-m-y');
-    // Upload image from temporary location on the server (folder we specify)
-    // move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $user_password = password_hash($user_password, PASSWORD_BCRYPT, array('cost' => 10));
 
     $query = "INSERT INTO users (user_firstname, user_lastname, user_role, username, user_email, user_password) ";
     $query .= "VALUES ('{$user_firstname}','{$user_lastname}','{$user_role}','{$username}','{$user_email}','{$user_password}') ";
