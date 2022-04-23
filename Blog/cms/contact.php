2f202c14d3d5fc2bc84576198331639c678921e8
pavelFaraday <devstudio.corp@gmail.com>
2022-04-23 16:10:37 +0400

    PHP Udemy - Blog | 254. Modifying Email Headers

--- Blog/cms/contact.php
+++ Blog/cms/contact.php
@@ -17,7 +17,7 @@ if (isset($_POST['submit'])) {
     $to      = "devstudio.corp@gmail.com";
     $subject = wordwrap($_POST['subject'], 70);
     $body    = $_POST['body'];
-    $header  = $_POST['email'];
+    $header  = "From: " . $_POST['email'];
 
     mail($to, $subject, $body, $header);
 }
