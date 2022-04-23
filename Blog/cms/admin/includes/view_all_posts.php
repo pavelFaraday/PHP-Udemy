372d17a54b69183ea0e086d725143c490930d6a0
pavelFaraday <devstudio.corp@gmail.com>
2022-04-23 16:52:00 +0400

    PHP Udemy - Blog | 257. Writing the code

--- Blog/cms/admin/includes/view_all_posts.php
+++ Blog/cms/admin/includes/view_all_posts.php
@@ -137,8 +137,7 @@ if (isset($_POST['checkBoxArray'])) {
                 echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
                 echo "<td><a href='../post.php?p_id={$post_id}' target='_blank'>View Post</a></td>";
                 echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-                echo "<td><a rel='$post_id' href='' class='delete_link'>Delete</a></td>";
-                // echo "<td><a onClick=\"javascript: return confirm('Are you sure?'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
+                echo "<td><a rel='$post_id' href='javascript:void(0)' class='delete_link'>Delete</a></td>";
                 echo "</tr>";
             }
 
@@ -172,7 +171,9 @@ if (isset($_GET['reset'])) {
     $(document).ready(function() {
         $(".delete_link").on("click", function() {
             var id = $(this).attr("rel");
-            alert(id);
+            var delete_url = "posts.php?delete=" + id + " ";
+            $(".modal_delete_link").attr("href", delete_url);
+            $("#myModal").modal("show");
         });
     });
 </script>
