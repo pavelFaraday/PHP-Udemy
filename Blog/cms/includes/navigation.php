8e420c1bc2c9a7af3267ef295313f24ea47d6d72
pavelFaraday <devstudio.corp@gmail.com>
2022-04-24 06:50:19 +0400

    PHP Udemy - Blog | Stop 263. Active navigation links

--- Blog/cms/includes/navigation.php
+++ Blog/cms/includes/navigation.php
@@ -26,14 +26,30 @@
                 while ($row = mysqli_fetch_assoc($resultSelect)) {
                     $cat_id = $row['cat_id'];
                     $cat_title = $row['cat_title'];
-                    echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
+
+                    $category_class = '';
+                    $registration_class = '';
+                    $contact_class = '';
+                    $registration = 'registration.php';
+                    $contact = 'contact.php';
+                    $pageName = basename($_SERVER['PHP_SELF']);
+
+                    if (isset($_GET['category']) && $_GET['category'] == $cat_id) {
+                        $category_class = 'active';
+                    } else if ($pageName == $registration) {
+                        $registration_class = 'active';
+                    } else if ($pageName == $contact) {
+                        $contact_class = 'active';
+                    }
+
+                    echo "<li class='$category_class'><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
                 }
 
                 ?>
 
                 <li><a href="admin">Admin</a></li>
-                <li><a href="registration.php">Registration</a></li>
-                <li><a href="contact.php">Contact</a></li>
+                <li class='<?php echo $registration_class; ?>'><a href="registration.php">Registration</a></li>
+                <li class='<?php echo $contact_class; ?>'><a href="contact.php">Contact</a></li>
 
                 <?php
                 if (isset($_SESSION['user_role'])) {
