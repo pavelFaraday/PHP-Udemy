b19f79d1dea69f5c213a890282dc86928bdf510e
pavelFaraday <devstudio.corp@gmail.com>
2022-04-24 05:22:24 +0400

    PHP Udemy - Blog | 260. No categories feature

--- Blog/cms/category.php
+++ Blog/cms/category.php
@@ -14,41 +14,47 @@
 
             if (isset($_GET['category'])) {
                 $post_category_id = $_GET['category'];
-            }
 
-            $query_SelectPosts = "SELECT * from posts WHERE post_category_id = $post_category_id ";
-            $result_SelectAllPosts = mysqli_query($connection, $query_SelectPosts);
+                $query_SelectPosts = "SELECT * from posts WHERE post_category_id = $post_category_id AND post_status='published' ";
+                $result_SelectAllPosts = mysqli_query($connection, $query_SelectPosts);
 
-            while ($row = mysqli_fetch_assoc($result_SelectAllPosts)) {
-                $post_id = $row['post_id'];
-                $post_title = $row['post_title'];
-                $post_author = $row['post_author'];
-                $post_date = $row['post_date'];
-                $post_image = $row['post_image'];
-                $post_content = substr($row['post_content'], 0, 150);
+                if (mysqli_num_rows($result_SelectAllPosts) < 1) {
+                    echo "<h1 class='text-center'>No Posts Available</h1>";
+                } else {
 
+                    while ($row = mysqli_fetch_assoc($result_SelectAllPosts)) {
+                        $post_id = $row['post_id'];
+                        $post_title = $row['post_title'];
+                        $post_author = $row['post_author'];
+                        $post_date = $row['post_date'];
+                        $post_image = $row['post_image'];
+                        $post_content = substr($row['post_content'], 0, 150);
             ?>
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+                        <h1 class="page-header">
+                            Page Heading
+                            <small>Secondary Text</small>
+                        </h1>
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php"><?php echo $post_author; ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                <hr>
-                <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                <hr>
+                        <!-- First Blog Post -->
+                        <h2>
+                            <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
+                        </h2>
+                        <p class="lead">
+                            by <a href="index.php"><?php echo $post_author; ?></a>
+                        </p>
+                        <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
+                        <hr>
+                        <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                        <hr>
+                        <p><?php echo $post_content; ?></p>
+                        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                        <hr>
 
-            <?php } ?>
+            <?php }
+                }
+            } else {
+                header("Location: index.php");
+            } ?>
 
         </div>
 
