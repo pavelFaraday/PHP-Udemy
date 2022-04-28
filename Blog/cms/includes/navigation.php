08b76e1fc63741434dc0f583213d4b706b31c6d8
pavelFaraday <devstudio.corp@gmail.com>
2022-04-28 19:54:46 +0400

    PHP Udemy - Blog | 291. Rewriting post page and query string

--- Blog/cms/includes/navigation.php
+++ Blog/cms/includes/navigation.php
@@ -11,7 +11,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php">Home</a>
+            <a class="navbar-brand" href="/PHP-UDEMY/Blog/cms">Home</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
